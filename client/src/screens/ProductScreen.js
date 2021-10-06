8a75466c9483862e223c3f2ba559f9fe00a37c50
Matthew Howard <mattalanhoward@gmail.com>
2021-10-06 05:46:51 -0400

    Get Product request from Client Setup Concurrently

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
 
-const ProductScreen = (props) => {
-  const product = products.find(
-    (product) => product._id === props.match.params.id
-  );
-  console.log(`product`, product);
+const ProductScreen = ({ match }) => {
+  const [product, setProduct] = useState([]);
+
+  //Makes request to backend on componenet load.
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [match]);
 
   return (
     <div>
